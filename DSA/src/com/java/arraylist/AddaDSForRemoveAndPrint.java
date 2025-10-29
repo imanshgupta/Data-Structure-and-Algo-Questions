505887403523075b39ec08b6dba28dec400c3a46
ANSH GUPTA <iamtheanshgupta@gmail.com>
2025-10-29 13:56:33 +0530

    Stack practice questions

---
+++ DSA/src/com/java/arraylist/AddaDSForRemoveAndPrint.java
@@ -0,0 +1,10 @@
+package com.java.arraylist;
+
+public class AddaDSForRemoveAndPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- DSA/src/com/java/arraylist/JosephsProblemCIrcularLinkedList.java
+++ DSA/src/com/java/arraylist/JosephsProblemCIrcularLinkedList.java
@@ -1,3 +1,4 @@
+package com.java.arraylist;
 import java.util.Iterator;
 import java.util.LinkedList;
 
